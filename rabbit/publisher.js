6a07397e712c49e7ae775336882e8b18ecefa0d2
Natalie <103411864+puppymatrix@users.noreply.github.com>
2024-03-19 18:52:03 +0800

    testing with email

--- rabbit/publisher.js
+++ rabbit/publisher.js
@@ -2,7 +2,7 @@ const { connect } = require('amqplib');
 
 const amqp = require('amqplib/callback_api');
 
-amqp.connect('amqp://localhost', (err, conn) => {
+amqp.connect('amqp://localhost:5672', (err, conn) => {
     if(err){
         throw err;
     }
@@ -10,8 +10,17 @@ amqp.connect('amqp://localhost', (err, conn) => {
         if(err){
             throw err;
         }
-        let queueName = "technical"
-        let message = "Hello World"
+        let queueName = "notifQueue"
+        let jsonMsg = {
+                emailType: "bookingConfirmation",
+                travelerEmail: ,
+                travelerName: "John",
+                hostEmail: "host@gmail.com",
+                hostName: "Mary",
+                bookingDates: "3 May",
+                totalPrice: "400",
+        }
+        let message = "Can i send json text"
         channel.assertQueue(queueName, {durable: false});
         channel.sendToQueue(queueName, Buffer.from(message));
         console.log(" [x] Sent %s", message);
