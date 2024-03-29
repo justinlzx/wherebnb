3ccbb09c89956161bb53400ae63afc63258188b9
justinlzx <justinlee01.jl@gmail.com>
2024-03-29 13:45:53 +0800

    WIP

--- rabbit/index.js
+++ rabbit/index.js
@@ -2,9 +2,11 @@ import express from 'express';
 import { routes } from './routes/index.js';
 import chalk from 'chalk';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 const app = express();
 
+dotenv.config()
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -22,7 +24,6 @@ app.use(
   ) => {
     console.error(err);
     const code = typeof err.code === "number" ? err.code : 500;
-   
   },
 );
 
